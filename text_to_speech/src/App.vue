93be8d5ea353ba668e795a5ef6a0be6297000435
SirEctor <ebenezer.teshome@duke.edu>
2024-08-09 17:27:05 -0400

    basic ui set

--- text_to_speech/src/App.vue
+++ text_to_speech/src/App.vue
@@ -4,44 +4,22 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div style="text-align: center; display:flex; flex-direction: column; height: 100vh; justify-content: center; align-items: center;"> 
+    <h1 style="font-size:6.5em;">Text To Speech Converter</h1>
+    <div style="display:flex;flex-direction:row;align-content:space-around;width:100%;justify-content:space-evenly">
+      <h2 style="font-size:4em;">Text:</h2>
+      <textarea style="width: 785px;height: 180px;" placeholder="Enter text here."></textarea>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    
+    <div style="display:flex;flex-direction:row;align-content:space-around;width:100%;justify-content:space-evenly;margin-top:20px;">
+      <h2 style="font-size:4em;">Voices:</h2>
+      <select>
+        <option>Test Voice 1</option>
+        <option>Test Voice 2</option>
+      </select>
+    </div>
+    
+    <button style="margin-top:20px;font-size:1em;">Generate</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
