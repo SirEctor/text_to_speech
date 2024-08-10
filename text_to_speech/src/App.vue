7422487f9a5e53dbe3b3a31d41697f621d1c1f8c
SirEctor <ebenezer.teshome@duke.edu>
2024-08-10 15:01:04 -0400

    api found

--- text_to_speech/src/App.vue
+++ text_to_speech/src/App.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+const count = ref(0)
+const theInputText = ref('')
+const chosenVoice = ref('')
+
+async function testFunc() {
+  console.log(theInputText.value);
+  console.log(chosenVoice.value);
+
+  try {
+    let resp = await fetch('https://api.streamelements.com/kappa/v2/speech?voice='+chosenVoice.value+'&text="' + theInputText.value +'"');
+    let mp3 = await resp.blob();
+    
+    let blobUrl = URL.createObjectURL(mp3);
+    document.getElementById('source').setAttribute('src', blobUrl);
+    let audio = document.getElementById('audio');
+    audio.pause();
+    audio.load();
+    audio.play();
+    
+  } catch(error){
+    console.log(error);
+  }
+}
 </script>
 
 <template>
@@ -8,18 +34,22 @@ import TheWelcome from './components/TheWelcome.vue'
     <h1 style="font-size:6.5em;">Text To Speech Converter</h1>
     <div style="display:flex;flex-direction:row;align-content:space-around;width:100%;justify-content:space-evenly">
       <h2 style="font-size:4em;">Text:</h2>
-      <textarea style="width: 785px;height: 180px;" placeholder="Enter text here."></textarea>
+      <textarea v-model="theInputText" style="width: 785px;height: 180px;" placeholder="Enter text here."></textarea>
     </div>
     
     <div style="display:flex;flex-direction:row;align-content:space-around;width:100%;justify-content:space-evenly;margin-top:20px;">
       <h2 style="font-size:4em;">Voices:</h2>
-      <select>
-        <option>Test Voice 1</option>
-        <option>Test Voice 2</option>
+      <select v-model="chosenVoice">
+        <option disabled value="">Please select one.</option>
+        <option>Brian</option>
+        <option>Ivy</option>
       </select>
     </div>
     
-    <button style="margin-top:20px;font-size:1em;">Generate</button>
+    <button @click="testFunc" style="margin-top:20px;font-size:3em;">Generate</button>
+    <audio id="audio" controls="">
+        <source id="source" type="audio/wav">
+    </audio>
+
   </div>
 </template>
-
